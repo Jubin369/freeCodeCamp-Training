ea3e18d115bad4a098fb4e19e1b3bd67927368ea
JuBin <jubinayoob369@gmail.com>
2018-02-04 15:04:55 +0530

    Create 225.Filter Arrays with filter.js

---
+++ Front End Development Certification/Javascript/225.Filter Arrays with filter.js
@@ -0,0 +1,10 @@
+//link:https://www.freecodecamp.org/challenges/filter-arrays-with-filter
+
+
+var oldArray = [1,2,3,4,5,6,7,8,9,10];
+
+// Only change code below this line.
+
+var newArray = oldArray.filter(function(val) {
+  return val <6;
+});
