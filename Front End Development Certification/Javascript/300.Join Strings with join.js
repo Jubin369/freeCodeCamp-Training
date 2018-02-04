6c86ce0ae8b441119ade1859505425b524f26d59
JuBin <jubinayoob369@gmail.com>
2018-02-04 15:20:17 +0530

    Create 300.Join Strings with join.js

---
+++ Front End Development Certification/Javascript/300.Join Strings with join.js
@@ -0,0 +1,9 @@
+//link:https://www.freecodecamp.org/challenges/join-strings-with-join
+
+
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+
+// Only change code below this line.
+
+joinedString = joinMe.join(" ");
