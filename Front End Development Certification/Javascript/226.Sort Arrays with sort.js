be232182946e2d8e6a77d95e0ec80dc5cba43b8a
JuBin <jubinayoob369@gmail.com>
2018-02-04 15:10:05 +0530

    Create 226.Sort Arrays with sort.js

---
+++ Front End Development Certification/Javascript/226.Sort Arrays with sort.js
@@ -0,0 +1,10 @@
+//link:https://www.freecodecamp.org/challenges/sort-arrays-with-sort
+
+
+var array = [1, 12, 21, 2];
+
+// Only change code below this line.
+
+array.sort(function(a, b) {
+  return b - a;
+});
