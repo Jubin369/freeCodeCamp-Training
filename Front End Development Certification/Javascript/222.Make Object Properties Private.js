99b9d01354712b5263e1a579d806d32c157363ad
JuBin <jubinayoob369@gmail.com>
2018-02-04 14:48:37 +0530

    Create 222.Make Object Properties Private.js

---
+++ Front End Development Certification/Javascript/222.Make Object Properties Private.js
@@ -0,0 +1,37 @@
+
+var Car = function() {
+  // this is a private variable
+  var speed = 10;
+
+  // these are public methods
+  this.accelerate = function(change) {
+    speed += change;
+  };
+
+  this.decelerate = function() {
+    speed -= 5;
+  };
+
+  this.getSpeed = function() {
+    return speed;
+  };
+};
+
+var Bike = function() {
+
+  var gear='N';
+
+  // these are public methods
+  this.setGear = function(change) {
+    gear = change;
+  };
+
+  this.getGear = function() {
+    return gear;
+  };
+
+};
+
+var myCar = new Car();
+
+var myBike = new Bike();
