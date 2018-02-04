225d86f057f49adebb36118f62714f8043d57d03
JuBin <jubinayoob369@gmail.com>
2018-02-04 14:17:37 +0530

    Create 216.Find Whitespace with Regular Expressions.js

---
+++ Front End Development Certification/Javascript/216.Find Whitespace with Regular Expressions.js
@@ -0,0 +1,16 @@
+//link:https://www.freecodecamp.org/challenges/find-whitespace-with-regular-expressions
+
+
+
+
+// Setup
+var testString = "How many spaces are there in this sentence?";
+
+// Only change code below this line.
+
+var expression = /\s+/g;  // Change this line
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var spaceCount = testString.match(expression).length;
