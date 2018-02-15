011579c1b8fa6dd9e68b75c2751ab1be94c8ee71
JuBin <jubinayoob369@gmail.com>
2018-02-15 16:33:39 +0530

    Create 257.Sum All Numbers in a Range_2.js

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/257.Sum All Numbers in a Range_2.js
@@ -0,0 +1,44 @@
+//Basic Code Solution:
+
+function sumAll(arr) {
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var temp = 0;
+    for (var i=min; i <= max; i++){
+        temp += i;
+    }
+  return(temp);
+}
+
+sumAll([1, 4]);
+
+
+
+
+//Intermediate Code Solution:
+
+function sumAll(arr) {
+  // Buckle up everything to one!
+
+  // Using ES6 arrow function (one-liner)
+  var sortedArr = arr.sort((a,b) => a-b);
+  var firstNum = arr[0];
+  var lastNum = arr[1];
+  // Using Arithmetic Progression summing formula
+
+  var sum = (lastNum - firstNum + 1) * (firstNum + lastNum) / 2;
+  return sum;
+}
+
+
+//Advanced Code Solution:
+
+function sumAll(arr) {
+    var sum = 0;
+    for (var i = Math.min(...arr); i <= Math.max(...arr); i++){
+        sum += i;
+    }
+  return sum;
+}
+
+sumAll([1, 4]);
