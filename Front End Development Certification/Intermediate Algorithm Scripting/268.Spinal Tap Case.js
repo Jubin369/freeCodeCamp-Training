fdcd348ed698fb20481fbdc1914253d227014fcf
JuBin <jubinayoob369@gmail.com>
2018-02-19 20:04:13 +0530

    Create 268.Spinal Tap Case.js

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/268.Spinal Tap Case.js
@@ -0,0 +1,41 @@
+//Basic Code Solution:
+function spinalCase(str) {
+  // Create a variable for the white space and underscores.
+  var regex = /\s+|_+/g;
+
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+  // Replace space and underscore with -
+  return str.replace(regex, '-').toLowerCase();
+}
+
+// test here
+spinalCase('This Is Spinal Tap');
+
+
+
+
+//Intermediate Code Solution:
+function spinalCase(str) {
+  // Replace low-upper case to low-space-uppercase
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+  // Split on whitespace and underscores and join with dash
+  return str.toLowerCase().split(/(?:_| )+/) .join('-');
+}
+
+// test here
+spinalCase('This Is Spinal Tap');
+
+
+
+//Advanced Code Solution:
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+
+  return str.split(/\s|_|(?=[A-Z])/).join('-').toLowerCase()
+}
+
+// test here
+spinalCase('This Is Spinal Tap');
