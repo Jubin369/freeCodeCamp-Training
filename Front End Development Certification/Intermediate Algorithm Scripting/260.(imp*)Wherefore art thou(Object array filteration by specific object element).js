305be0cad993e4b221681f04f8f3b11a1d5d9006
JuBin <jubinayoob369@gmail.com>
2018-02-19 16:32:12 +0530

    Create 260.(imp*)Wherefore art thou(Object array filteration by specific object element).js

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/260.(imp*)Wherefore art thou(Object array filteration by specific object element).js
@@ -0,0 +1,54 @@
+//Basic Code Solution:
+
+function whatIsInAName(collection, source) {
+  
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    for(var i = 0; i < srcKeys.length; i++) {
+      if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
+
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
+
+
+//Intermediate Code Solution:
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function (obj) {
+    return srcKeys.every(function (key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+// test here
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+
+
+//Advanced Code Solution:
+function whatIsInAName(collection, source) {
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function (obj) {
+    return srcKeys
+      .map(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce(function(a, b) {
+        return a && b;
+      });
+  });
+}
+
+// test here
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
