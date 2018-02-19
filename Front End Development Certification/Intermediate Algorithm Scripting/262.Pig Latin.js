17c3276f28522df5bb6dfbd495d9db2349be34ad
JuBin <jubinayoob369@gmail.com>
2018-02-19 18:05:19 +0530

    Create 262.Pig Latin.js

---
+++ Front End Development Certification/Intermediate Algorithm Scripting/262.Pig Latin.js
@@ -0,0 +1,90 @@
+//Basic Code Solution:
+function translatePigLatin(str) {
+  // Create variables to be used
+  var pigLatin = '';
+  var regex = /[aeiou]/gi;
+
+  // Check if the first character is a vowel
+  if (str[0].match(regex)) {
+    pigLatin = str + 'way';
+
+  } else {
+
+    // Find how many consonants before the first vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
+  }
+
+  return pigLatin;
+}
+
+// test here
+translatePigLatin("ckkkkkkkkkkkonsonant");
+
+
+
+
+
+//Intermediate Code Solution:
+function translatePigLatin(str) {
+  function check(obj) {
+      return ['a','i','u','e','o'].indexOf(str.charAt(obj)) == -1 ? check(obj + 1) : obj;
+  }
+
+  return str.substr(check(0)).concat((check(0) === 0 ? 'w' : str.substr(0, check(0))) + 'ay');
+}
+
+// test here
+translatePigLatin("consonant");
+
+
+
+
+
+
+
+
+//Advanced Code Solution:
+function translatePigLatin(str) {
+    var strArr = [];
+    var tmpChar;
+
+    // check if the char is consonant using RegEx
+    function isConsonant(char) {
+        return !/[aeiou]/.test(char);
+    }
+
+    // return initial str + "way" if it starts with vowel
+    // if not - convert str to array
+    if (!isConsonant(str.charAt(0)))
+        return str + "way";
+    else
+        strArr = str.split("");
+
+    // push all consonats to the end of the array
+    while (isConsonant(strArr[0])) {
+        tmpChar = strArr.shift();
+        strArr.push(tmpChar);
+    }
+ // convert array to string and concatenate "ay" at the end  
+ return strArr.join("")+"ay";
+}
+
+// test here
+translatePigLatin("consonant");
+
+
+
+
+// Advanced Code Solution 2:
+function translatePigLatin(str) {
+  return str
+    .replace(/^([aeiouy])(.*)/, '$1$2way')
+    .replace(/^([^aeiouy]+)(.*)/, '$2$1ay');
+}
+
+// test here
+translatePigLatin("consonant");
