7d9562a009ec82ef93cdb5a652febf63c8f9e878
JuBin <jubinayoob369@gmail.com>
2018-04-09 14:11:51 +0530

    Update 02.Start a Node.js Server.js

--- Back End Development Certification/Node.js and Express.js/02.Start a Node.js Server.js
+++ Back End Development Certification/Node.js and Express.js/02.Start a Node.js Server.js
@@ -71,3 +71,43 @@ const lines = contents.toString().split('\n').length - 1
 console.log(lines)
 
 
+
+
+
+//4th assgn:MY FIRST ASYNC I/O!
+/*
+The solution to this problem is almost the same as the previous problem  
+  except you must now do it the Node.js way: asynchronous.  
+   
+  Instead of fs.readFileSync() you will want to use fs.readFile() and  
+  instead of using the return value of this method you need to collect the  
+  value from a callback function that you pass in as the second argument. To  
+  learn more about callbacks, check out:  
+  (https://github.com/maxogden/art-of-node#callbacks).  
+   
+  Remember that idiomatic Node.js callbacks normally have the signature:  
+   
+     function callback (err, data) {...}  
+   
+  so you can check if an error occurred by checking whether the first  
+  argument is truthy. If there is no error, you should have your Buffer  
+  object as the second argument. As with readFileSync(), you can supply  
+  'utf8' as the second argument and put the callback as the third argument  
+  and you will get a String instead of a Buffer.  
+   
+  Documentation on the fs module can be found by pointing your browser here:  
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyou  
+  node/node_apidoc/fs.html
+*/
+const fs = require('fs')
+const file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+  if (err) {
+    return console.log(err)
+  }
+  // fs.readFile(file, 'utf8', callback) can also be used
+  const lines = contents.toString().split('\n').length - 1
+  console.log(lines)
+})
+
