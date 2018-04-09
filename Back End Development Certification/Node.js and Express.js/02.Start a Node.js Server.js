91e9a4bf8eecc2c83170647381bd0cbd797cd730
JuBin <jubinayoob369@gmail.com>
2018-04-09 15:23:20 +0530

    Update 02.Start a Node.js Server.js

--- Back End Development Certification/Node.js and Express.js/02.Start a Node.js Server.js
+++ Back End Development Certification/Node.js and Express.js/02.Start a Node.js Server.js
@@ -13,6 +13,8 @@
   To learn more about callbacks, check out:  
   (https://github.com/maxogden/art-of-node#callbacks).  
   
+  Documentation on the http module can be found by pointing your browser here:  
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyounode/node_apidoc/http.html
 
 */
 
@@ -270,3 +272,48 @@ module.exports = function (dir, filterStr, callback) {
     callback(null, list)
   })
 }
+
+
+//7th assgn:HTTP Client
+/*
+   Write a program that performs an HTTP GET request to a URL provided to you  
+  as the first command-line argument. Write the String contents of each  
+  "data" event from the response to a new line on the console (stdout).  
+   
+ ─────────────────────────────────────────────────────────────────────────────  
+   
+ ## HINTS  
+   
+  For this exercise you will need to use the http core module.    
+   
+  The http.get() method is a shortcut for simple GET requests, use it to  
+  simplify your solution. The first argument to http.get() can be the URL  
+  you want to GET; provide a callback as the second argument.  
+   
+  Unlike other callback functions, this one has the signature:  
+   
+     function callback (response) {  ... }  
+   
+  Where the response object is a Node Stream object. You can treat Node  
+  Streams as objects that emit events. The three events that are of most  
+  interest are: "data", "error" and "end". You listen to an event like so:  
+   
+     response.on("data", function (data) {  ...  })  
+   
+  The "data" event is emitted when a chunk of data is available and can be  
+  processed. The size of the chunk depends upon the underlying data source.  
+   
+  The response object / Stream that you get from http.get() also has a  
+  setEncoding() method. If you call this method with "utf8", the "data"  
+  events will emit Strings rather than the standard Node Buffer objects  
+  which you have to explicitly convert to Strings.
+
+*/
+
+const http = require('http')
+
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+}).on('error', console.error)
