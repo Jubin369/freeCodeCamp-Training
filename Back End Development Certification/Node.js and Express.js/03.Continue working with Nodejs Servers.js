3a851fd8bc2dd854c2dc97fbb59ef52f6cada56b
JuBin <jubinayoob369@gmail.com>
2018-04-10 09:53:56 +0530

    Update 03.Continue working with Nodejs Servers.js

--- Back End Development Certification/Node.js and Express.js/03.Continue working with Nodejs Servers.js
+++ Back End Development Certification/Node.js and Express.js/03.Continue working with Nodejs Servers.js
@@ -1,6 +1,16 @@
 //Documentation
 /*
-<https://npmjs.com/bl> <https://npmjs.com/concat-stream>
+  <https://npmjs.com/bl> <https://npmjs.com/concat-stream>
+  
+  Documentation for both of these modules has been installed along with  
+  learnyounode on your system and you can read them by pointing your browser  
+  here:  
+   
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyounode/docs/bl.html  
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyounode/docs/concat-stream.html
+  
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyounode/node_modules/bl  
+  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyounode/node_modules/concat-stream
 */
 //8th assgn: HTTP COLLECT
 
@@ -49,29 +59,30 @@ Write a program that performs an HTTP GET request to a URL provided to you
   directory and copy the entire directory for the package you want to use  
   from inside the learnyounode installation directory:  
    
-  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyou  
-  node/node_modules/bl  
-  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyou  
-  node/node_modules/concat-stream  
+    
    
   Both bl and concat-stream can have a stream piped in to them and they will  
   collect the data for you. Once the stream has ended, a callback will be  
   fired with the data:  
    
-     response.pipe(bl(function (err, data) { /* ... */ }))  
+     response.pipe(bl(function (err, data) { ...  }))  
      // or  
-     response.pipe(concatStream(function (data) { /* ... */ }))  
+     response.pipe(concatStream(function (data) {  ...  }))  
    
   Note that you will probably need to data.toString() to convert from a  
-  Buffer.  
-   
-  Documentation for both of these modules has been installed along with  
-  learnyounode on your system and you can read them by pointing your browser  
-  here:  
-   
-  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyou  
-  node/docs/bl.html  
-  file:///home/ec2-user/.nvm/versions/node/v6.13.1/lib/node_modules/learnyou  
-  node/docs/concat-stream.html  
+  Buffer.    
 
 */
+const http = require('http')
+const bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
