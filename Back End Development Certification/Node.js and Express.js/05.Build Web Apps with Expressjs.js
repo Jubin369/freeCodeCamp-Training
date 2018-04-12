3ba267a77f6d4a0a07fc27fb3a89b52e5593662d
JuBin <jubinayoob369@gmail.com>
2018-04-12 14:55:12 +0530

    Update 05.Build Web Apps with Expressjs.js

--- Back End Development Certification/Node.js and Express.js/05.Build Web Apps with Expressjs.js
+++ Back End Development Certification/Node.js and Express.js/05.Build Web Apps with Expressjs.js
@@ -293,3 +293,34 @@ app.put('/message/:id', function(req, res){
 })
 
 app.listen(process.argv[2])
+
+
+//7th assgn: WHAT'S IN QUERY
+/*
+
+Write a route that extracts data from query string in the GET '/search' URL
+route, e.g. ?results=recent&include_tabs=true and then outputs it back to
+the user in JSON format.
+
+-------------------------------------------------------------------------------
+
+## HINTS
+
+In Express.js to extract query string parameters, we can use:
+
+    req.query.NAME
+
+To output JSON we can use:
+
+    res.send(object)
+
+*/
+var express = require('express')
+var app = express()
+
+app.get('/search', function(req, res){
+  var query = req.query
+  res.send(query)
+})
+
+app.listen(process.argv[2])
