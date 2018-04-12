e80b34c62c555c60f302f774ff4cf4b14042a729
JuBin <jubinayoob369@gmail.com>
2018-04-12 11:15:19 +0530

    Create 05.Build Web Apps with Expressjs.js

---
+++ Back End Development Certification/Node.js and Express.js/05.Build Web Apps with Expressjs.js
@@ -0,0 +1,29 @@
+//Documentation
+/*
+
+
+*/
+
+//1st assgn: HELLO WORLD!
+/*
+This is how we can create an Express.js app on port 3000, that responds with
+a string on '/':
+
+    var express = require('express')
+    var app = express()
+    app.get('/', function(req, res) {
+      res.end('Hello World!')
+    })
+    app.listen(3000)
+
+Please use process.argv[2] instead of a fixed port number:
+
+    app.listen(process.argv[2])
+
+*/
+var express = require('express')
+var app = express()
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
+})
+app.listen(process.argv[2])
