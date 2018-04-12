676a13fb4cacf837810268818c34b69d556dc9b9
JuBin <jubinayoob369@gmail.com>
2018-04-12 14:47:36 +0530

    Update 05.Build Web Apps with Expressjs.js

--- Back End Development Certification/Node.js and Express.js/05.Build Web Apps with Expressjs.js
+++ Back End Development Certification/Node.js and Express.js/05.Build Web Apps with Expressjs.js
@@ -249,3 +249,47 @@ app.use(express.static(process.argv[3]));
 
 
 app.listen(process.argv[2])
+
+
+//6th assgn: PARAM PAM PAM
+/*
+Create an Express.js server that processes PUT '/message/:id' requests.
+
+For instance:
+
+    PUT /message/526aa677a8ceb64569c9d4fb
+
+As a response to these requests, return the SHA1 hash of the current date
+plus the sent ID:
+
+    require('crypto')
+      .createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex')
+
+-------------------------------------------------------------------------------
+
+## HINTS
+
+To handle PUT requests use:
+
+    app.put('/path/:NAME', function(req, res){...});
+
+To extract parameters from within the request handlers, use:
+
+    req.params.NAME
+
+*/
+var express = require('express')
+var app = express()
+
+app.put('/message/:id', function(req, res){
+  var id = req.params.id
+  var str = require('crypto')
+    .createHash('sha1')
+    .update(new Date().toDateString() + id)
+    .digest('hex')
+  res.send(str)
+})
+
+app.listen(process.argv[2])
