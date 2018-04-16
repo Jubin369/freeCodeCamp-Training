f1eb20069b2328518e68c7ea012c4948ecc4e8d3
Jubin Ayoob <jubinayoob369@gmail.com>
2018-04-16 23:51:05 +0530

    Update 09.Store Data in MongoDB.js

--- Back End Development Certification/MongoDB/09.Store Data in MongoDB.js
+++ Back End Development Certification/MongoDB/09.Store Data in MongoDB.js
@@ -1,9 +1,13 @@
 //Documentation
 /*
 ## Resources:
-
+ 3rd:
   * http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html#find
   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+ 4th
+  * http://docs.mongodb.org/manual/reference/method/db.collection.find/#explicitly-exclude-the-id-field
+  * http://mongodb.github.io/node-mongodb-native/2.2/api/Collection.html#find
+
 
 
 */
@@ -73,6 +77,68 @@ mongo.connect(url, function(err, client) {
       $gt: +age
     }
   }).toArray(function(err, docs) {
+    if (err) throw err
+    console.log(docs)
+    client.close()
+  })
+})
+
+//4th assgn: FIND PROJECT
+/*
+Here we will learn how to search for documents but only fetch the fields
+we need. Also known as projection in MongoDB
+
+Use the parrots collection from the database named learnyoumongo to
+find all documents where age is greater than the first argument
+passed to your script.
+
+The difference from the last lesson will be that we only want the
+name and age properties
+
+Using console.log, print the documents to stdout.
+
+-------------------------------------------------------------------------------
+
+## HINTS
+
+To find a document or documents, one needs to call find() on the collection.
+
+Find is a little bit different than what we are used to seeing.
+
+Here is an example:
+
+    collection.find({
+      name: 'foo'
+    }, {
+      name: 1
+    , age: 1
+    , _id: 0
+    }).toArray(function(err, documents) {
+    
+    })
+
+If your program does not finish executing, you may have forgotten to
+close the db. That can be done by calling db.close() after you
+have finished.
+
+*/
+var mongo = require('mongodb').MongoClient
+var age = process.argv[2]
+
+var url = 'mongodb://localhost:27017'
+
+// Database Name
+const dbName = 'learnyoumongo';
+
+mongo.connect(url, function(err, client) {
+  if (err) throw err;
+  const db = client.db(dbName);
+  const parrots = db.collection('parrots')
+  parrots.find({
+    age: {
+      $gt: +age
+    }
+  },{ _id: 0, name: 1, age: 1}).toArray(function(err, docs) {
     if (err) throw err
     console.log(docs)
     client.close()
