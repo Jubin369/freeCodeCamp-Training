0f498d219e8bf4c068a507d89c43dd364498aad2
Jubin Ayoob <jubinayoob369@gmail.com>
2018-04-24 17:52:27 +0530

    Update 234.Check for Palindromes.js

--- Front End Development Certification/Basic Algorithm Scripting/234.Check for Palindromes.js
+++ Front End Development Certification/Basic Algorithm Scripting/234.Check for Palindromes.js
@@ -15,6 +15,52 @@ function palindrome(str) {
   return true;
 }
 
-
-
 palindrome("A man, a plan, a canal. Panama");
+
+
+//Basic Code Solution:
+function palindrome(str) {
+  return str.replace(/[\W_]/g, '').toLowerCase() ===
+         str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+}
+
+
+//Intermediate Code Solution:
+function palindrome(str) {
+  str = str.toLowerCase().replace(/[\W_]/g, '');
+  for(var i = 0, len = str.length - 1; i < len/2; i++) {
+    if(str[i] !== str[len-i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Advanced Code Solution (most performant):
+function palindrome(str) {
+  //assign a front and a back pointer
+  let front = 0;
+  let back = str.length - 1;
+
+  //back and front pointers won't always meet in the middle, so use (back > front)
+  while (back > front) {
+    //increments front pointer if current character doesn't meet criteria
+    while ( str[front].match(/[\W_]/) ) {
+      front++;
+      continue;
+    }
+    //decrements back pointer if current character doesn't meet criteria
+    while ( str[back].match(/[\W_]/) ) {
+      back--;
+      continue;
+    }
+    //finally does the comparison on the current character
+    if ( str[front].toLowerCase() !== str[back].toLowerCase() ) return false
+    front++;
+    back--;
+  }
+  
+  //if the whole string has been compared without returning false, it's a palindrome!
+  return true;
+
+}
