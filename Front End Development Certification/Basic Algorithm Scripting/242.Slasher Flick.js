30eaabb569586013bb92c656e30b49824ac747b6
Jubin Ayoob <jubinayoob369@gmail.com>
2018-04-25 11:54:08 +0530

    Update 242.Slasher Flick.js

--- Front End Development Certification/Basic Algorithm Scripting/242.Slasher Flick.js
+++ Front End Development Certification/Basic Algorithm Scripting/242.Slasher Flick.js
@@ -1,7 +1,17 @@
-
+//my solution
 function slasher(arr, howMany) {
   // it doesn't always pay to be first
   return arr.splice(howMany);
 }
 
+slasher([1, 2, 3], 2);
+
+
+//Basic Code Solution:
+function slasher(arr, howMany) {
+  // remove the head
+  arr.splice(0, howMany);
+  // return the remaining or the tail
+  return arr;
+}
 slasher([1, 2, 3], 2);
