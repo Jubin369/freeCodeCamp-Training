bf4c0c04a7db10259a38584b4e488692933bba5c
Jubin Ayoob <jubinayoob369@gmail.com>
2018-04-25 11:13:26 +0530

    Update 240.Truncate a string.js

--- Front End Development Certification/Basic Algorithm Scripting/240.Truncate a string.js
+++ Front End Development Certification/Basic Algorithm Scripting/240.Truncate a string.js
@@ -1,4 +1,4 @@
-
+//my solution
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   var n=num;
@@ -8,5 +8,25 @@ function truncateString(str, num) {
     str=str.slice(0,n)+"...";
   return str;
 }
-
 truncateString("Absolutely Longer", 2);
+
+
+//Basic Code Solution:
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+}
+
+
+//Advanced Code Solution:
+function truncateString(str, num) {
+  if (str.length > num)
+    return str.slice(0, num > 3 ? num-3 : num) + '...';
+  return str;
+}
