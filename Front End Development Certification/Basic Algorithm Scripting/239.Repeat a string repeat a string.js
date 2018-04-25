dd7d46fa82465cf174cdd76a23a71c101f357791
Jubin Ayoob <jubinayoob369@gmail.com>
2018-04-25 11:04:17 +0530

    Update 239.Repeat a string repeat a string.js

--- Front End Development Certification/Basic Algorithm Scripting/239.Repeat a string repeat a string.js
+++ Front End Development Certification/Basic Algorithm Scripting/239.Repeat a string repeat a string.js
@@ -1,4 +1,4 @@
-
+//my solution
 function repeatStringNumTimes(str, num) {
   var res="";
   while(num>0){
@@ -7,5 +7,23 @@ function repeatStringNumTimes(str, num) {
   }
   return res;
 }
+repeatStringNumTimes("abc", 3);
 
+
+//Intermediate Code Solution:
+function repeatStringNumTimes(str, num) {
+  if(num < 0)
+    return "";
+  if(num === 1)
+    return str;
+  else
+    return str + repeatStringNumTimes(str, num - 1);
+}
+repeatStringNumTimes("abc", 3);
+
+
+//Advanced Code Solution:
+function repeatStringNumTimes(str, num) {
+  return num > 0 ? str.repeat(num) : '';
+}
 repeatStringNumTimes("abc", 3);
