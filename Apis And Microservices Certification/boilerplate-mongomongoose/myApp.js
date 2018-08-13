69f19d31eb1d2c9d49305ede201ccbb1be745fcb
JuBinAyoob <jubinayoob369@gmail.com>
2018-08-13 23:08:06 +0530

    FCC-Apis And Microservices Certification: EX:3.05(find query)

--- Apis And Microservices Certification/boilerplate-mongomongoose/myApp.js
+++ Apis And Microservices Certification/boilerplate-mongomongoose/myApp.js
@@ -123,9 +123,9 @@ var createManyPeople = function(arrayOfPeople, done) {
 // Use the function argument `personName` as search key.
 
 var findPeopleByName = function(personName, done) {
-  
-  done(null/*, data*/);
-
+  Person.find({name:personName},function(err,data){
+      done(null, data);
+  });
 };
 
 /** 6) Use `Model.findOne()` */
