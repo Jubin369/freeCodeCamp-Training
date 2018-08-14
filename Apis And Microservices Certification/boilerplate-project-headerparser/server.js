7796b8499095ec7eb0568f88b99449a3d0faa184
JuBinAyoob <jubinayoob369@gmail.com>
2018-08-14 12:34:50 +0530

    FCC-Apis And Microservices Certification: EX:4.02(whoami;solution)

--- Apis And Microservices Certification/boilerplate-project-headerparser/server.js
+++ Apis And Microservices Certification/boilerplate-project-headerparser/server.js
@@ -24,7 +24,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+// whoami API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  res.json({ipaddress: req.ip,language:req.headers["accept-language"],software:req.get('User-Agent')});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
