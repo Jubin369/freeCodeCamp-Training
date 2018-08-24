9563325998b8c73e9ef6807b116b5aa72be3c63d
Jubin Ayoob <jubinayoob369@gmail.com>
2018-08-24 23:44:26 +0530

    Create 20.Review Using Props with Stateless Functional Components.js

---
+++ Front End Development Certification/React/20.Review Using Props with Stateless Functional Components.js
@@ -0,0 +1,24 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
+const Camper = (props) =>{
+    return( <div>
+              <p>{props.name}</p>
+            </div>);
+};
+
+Camper.defaultProps = {
+   name: 'CamperBot'
+};
+
+Camper.propTypes = { name: PropTypes.string.isRequired };
